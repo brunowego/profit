9f1e26a3cd0f56f3bb93665a3cab4b272e4fa9e1
Bruno Wego <brunowego@gmail.com>
2022-07-23 11:50:38 -0300

    chore(code): evolve basic functionality to verify rsi

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const TradingView = require('@mathieuc/tradingview')
-const notifier = require('node-notifier');
+const notifier = require('node-notifier')
+const fs = require('fs-extra')
+const os = require('os')
+const path = require('path')
+const YAML = require('yaml')
+const chalk = require('chalk')
+const humanizeDuration = require('humanize-duration');
 
-const run = async () => {
+const rcPath = path.resolve(os.homedir(), '.profitrc.yaml')
+
+yaml = fs.readFileSync(rcPath, 'utf8')
+config = YAML.parse(yaml)
+frequency = config.alert.frequency * 1000
+rsi = config.watch.rsi
+
+const checkRSI = async (m) => {
   const client = new TradingView.Client()
-
   const chart = new client.Session.Chart()
-  chart.setMarket('BINANCE:ETCUSDTPERP', {
-    timeframe: '15',
+
+  chart.setMarket(`${m.source}:${m.symbol}`, {
+    timeframe: m.timeframe.toString(),
     range: 100
   })
 
@@ -16,24 +29,46 @@ const run = async () => {
 
   chartStudy.onUpdate(() => {
     const latestPeriod = chartStudy.periods[0]
-    console.log(`RSI: ${latestPeriod['$time']} ${latestPeriod['RSI']}`)
+    // console.log(`RSI: ${latestPeriod['$time']} ${latestPeriod['RSI']}`)
 
-    if (latestPeriod['RSI'] > 69) {
+    if (latestPeriod['RSI'] < rsi.oversold) {
       notifier.notify({
         title: 'Profit Alert',
-        message: `ETCUSDTPERP in Oversold`,
+        message: `${m.source}:${m.symbol} in Oversold`,
         sound: true
-      });
+      })
     }
 
-    if (latestPeriod['RSI'] < 31) {
+    if (latestPeriod['RSI'] > rsi.overbought) {
       notifier.notify({
         title: 'Profit Alert',
-        message: `ETCUSDTPERP in Overbought`,
+        message: `${m.source}:${m.symbol} in Overbought`,
         sound: true
-      });
+      })
     }
+
+    client.end()
+  })
+};
+
+console.log(chalk.green('Checking RSI every %s.'), humanizeDuration(frequency));
+console.log(chalk.green('Verification cycle started!'))
+
+const eachMarket = () => {
+  rsi.markets.forEach((m, index) => {
+    setTimeout(() => {
+      checkRSI(m)
+    }, 1000 * (index + 1))
   })
 }
 
-run()
+eachMarket()
+
+;(function loop() {
+  setTimeout(() => {
+    eachMarket()
+    loop()
+  }, frequency);
+}());
+
+console.log('Running...')
